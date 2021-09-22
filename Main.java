54c041c2f7a392049820ef4ef9d2dfdc91aacd3e
schielrh <89650296+schielrh@users.noreply.github.com>
2021-09-22 03:00:59 +0000

    Added println and loops

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
+/*
+Lab 4
+Solo: Ryan Schiell 
+Date: 9/21/21
+*/
+
+//this imports multiple Java utilities
+import java.util.*;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Random r = new Random();
+    int randomNum = r.nextInt(50);
+    randomNum += 1;
+
+    System.out.println("The Random number is: " + randomNum);
+
+    //here is the countdown loop
+    for (int numCountdown = randomNum; numCountdown >= 0; numCountdown--){
+      System.out.println(numCountdown);
+    }
+
+    //this will show the println outputs
+    if (randomNum <= 5){
+      System.out.println("Ahoy mateys!");
+    }
+    else if (randomNum > 25 && randomNum < 42){
+      System.out.println("Cannonball!");
+    }
+    else {
+      System.out.println("Blast off!");
+    }
   }
 }
